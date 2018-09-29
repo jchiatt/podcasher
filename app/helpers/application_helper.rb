0c72117aac1150eb10bac60767d1220046233909
J.C. Hiatt <jc@jchiatt.com>
2018-09-28 21:38:25 -0500

    run user migrations, add bootstrap flash class helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  # https://coderwall.com/p/jzofog/ruby-on-rails-flash-messages-with-bootstrap
+  FLASH_BOOTSTRAP_CLASS_DICTIONARY = {
+    notice:   'alert-info',
+    success:  'alert-success',
+    alert:    'alert-warning',
+    error:    'alert-danger'
+  }.freeze
+
+  def flash_bootstrap_class(key)
+    FLASH_BOOTSTRAP_CLASS_DICTIONARY[key.to_sym]
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_29_004004) do
+ActiveRecord::Schema.define(version: 2018_09_29_023630) do
 
   create_table "shows", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,18 @@ ActiveRecord::Schema.define(version: 2018_09_29_004004) do
     t.decimal "monthly_expenses"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end

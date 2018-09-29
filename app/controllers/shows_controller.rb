82cdaf7c9ac3e3915b58864835bda274b44ebdb3
J.C. Hiatt <jc@jchiatt.com>
2018-09-28 22:15:45 -0500

    add marketing page controller, add home page

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,4 +1,5 @@
 class ShowsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_show, only: [:show, :edit, :update, :destroy]
 
   # GET /shows
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :shows
-  root to: 'shows#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/pages/:page" => "pages#show"
+  root to: 'pages#show', page: 'home'
 end
